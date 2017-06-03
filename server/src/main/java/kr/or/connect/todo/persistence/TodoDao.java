08c02c5361b16355c27b3e02b0cb519a07f1de78
DESKTOP-BB2KIO6\nohjunho <github.com/gaepury>
2017-06-03 15:35:23 +0900

    Sql 쿼리 String 이동 및 수정

--- server/src/main/java/kr/or/connect/todo/persistence/TodoDao.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoDao.java
@@ -22,11 +22,6 @@ public class TodoDao {
 	private NamedParameterJdbcTemplate jdbc;
 	private SimpleJdbcInsert insertAction;
 	private RowMapper<Todo> rowMapper = BeanPropertyRowMapper.newInstance(Todo.class);
-	private static final String COUNT_TODO = "SELECT COUNT(*) FROM todo";
-	private static final String SELECT_BY_ID = "SELECT id, todo, completed, date FROM todo where id = :id";
-	private static final String SELECT_ALL = "SELECT id, todo, completed, date FROM todo";
-	private static final String DELETE_BY_ID = "DELETE FROM todo WHERE id= :id";
-	private static final String UPDATE = "UPDATE todo SET\n" + "todo = :todo," + "completed = :completed," + "date = :date\n" + "WHERE id = :id";
 
 	public TodoDao(DataSource dataSource) {
 		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
@@ -36,13 +31,13 @@ public class TodoDao {
 	// 할일 갯수
 	public int countTodos() {
 		Map<String, Object> params = Collections.emptyMap();
-		return jdbc.queryForObject(COUNT_TODO, params, Integer.class);
+		return jdbc.queryForObject(TodoSqls.COUNT_TODO, params, Integer.class);
 	}
 
 	// 모든 할일 리스트
 	public List<Todo> selectAll() {
 		Map<String, Object> params = Collections.emptyMap();
-		return jdbc.query(SELECT_ALL, params, rowMapper);
+		return jdbc.query(TodoSqls.SELECT_ALL, params, rowMapper);
 	}
 
 	// 해당 id의 할일
@@ -50,7 +45,7 @@ public class TodoDao {
 		RowMapper<Todo> rowMapper = BeanPropertyRowMapper.newInstance(Todo.class);
 		Map<String, Object> params = new HashMap<>();
 		params.put("id", id);
-		return jdbc.queryForObject(SELECT_BY_ID, params, rowMapper);
+		return jdbc.queryForObject(TodoSqls.SELECT_BY_ID, params, rowMapper);
 	}
 
 	// 할일 추가
@@ -62,13 +57,13 @@ public class TodoDao {
 	// 할일 수정
 	public int update(Todo Todo) {
 		SqlParameterSource params = new BeanPropertySqlParameterSource(Todo);
-		return jdbc.update(UPDATE, params);
+		return jdbc.update(TodoSqls.UPDATE, params);
 	}
 
 	// 할일 삭제
 	public int deleteById(Integer id) {
 		Map<String, ?> params = Collections.singletonMap("id", id);
-		return jdbc.update(DELETE_BY_ID, params);
+		return jdbc.update(TodoSqls.DELETE_BY_ID, params);
 	}
 
 }
